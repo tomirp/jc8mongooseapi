5eebd20a47498876c62592dec650450826f4478b
tomirp <tomirpdevelopment@gmail.com>
2019-04-30 10:54:36 +0700

    fix

--- src/config/mongose.js
+++ src/config/mongose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://tommy:27Leviathan_91@tommycluster-yfhlt.gcp.mongodb.net/jc8Mongoose?retryWrites=true', { // Generate connection to database
+mongoose.connect('mongodb+srv://tommy:27Leviathan_91@tommycluster-yfhlt.gcp.mongodb.net/jc8mongoose?retryWrites=true', { // Generate connection to database
     useNewUrlParser: true, // Parsing URL to the form mongodb needs
     useCreateIndex: true, // Auto generate Indexes from mongodb, to get access to the data
     useFindAndModify: false //  deprecated
